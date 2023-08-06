77f1e93cfaa6484e8209baa4138e69798a7b0483
bdjones01 <britzay1009@gmail.com>
2023-08-06 02:44:10 +0000

    Week20 project

--- main.tf
+++ main.tf
@@ -17,8 +17,10 @@ resource "aws_instance" "jenkins_instance" {
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install openjdk-11-jre -y
-sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] 
+https://pkg.jenkins.io/debian binary | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install fontconfig openjdk-11-jre -y
 sudo apt-get install jenkins -y
