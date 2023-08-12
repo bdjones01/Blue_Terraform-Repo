75efbc21083ab87ba1c6d6834bc1893fa337b854
bdjones01 <britzay1009@gmail.com>
2023-08-12 03:27:38 +0000

    variables

--- variables.tf
+++ variables.tf
@@ -9,9 +9,8 @@ variable "vpc_name" {
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
@@ -28,24 +27,4 @@ variable "public_subnets" {
     "public_subnet_2" = 2
     "public_subnet_3" = 3
   }
-}
-variable "variables_sub_cidr" {
-  description = "CIDR Block for the Variables Subnet"
-  type        = string
-  default     = "10.0.202.0/24"
-}
-variable "variables_sub_az" {
-  description = "Availability Zone used Variables Subnet"
-  type        = string
-  default     = "us-east-1a"
-}
-variable "variables_sub_auto_ip" {
-  description = "Set Automatic IP Assigment for Variables Subnet"
-  type        = bool
-  default     = true
-}
-variable "environment" {
-  description = "Environment for deployment"
-  type        = string
-  default     = "dev"
 }
